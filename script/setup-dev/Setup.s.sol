b2ff7bcefc8844e9aa2d5c66bea2e923eb720903
qTeePie <isabel@chaparian.no>
2025-12-12 23:56:07 +0100

    github workflows - removed fmt --check

--- script/setup-dev/Setup.s.sol
+++ script/setup-dev/Setup.s.sol
@@ -9,18 +9,20 @@ import {console} from "forge-std/console.sol";
 // local contracts
 import {OrderEngine} from "orderbook/OrderEngine.sol";
 
+// TODO: cryptopunk is not erc721 compatible, wrapper?
 interface IERC721 {
     function setApprovalForAll(address operator, bool approved) external;
+    function ownerOf(uint256 tokenId) external;
 }
 
 contract Setup is Script, Config, Test {
     OrderEngine public orderEngine;
 
     function run() external {
-        uint chainId = block.chainid;
-        console.log("Deploying to chain: ", chainId);
-
         _loadConfig("deployments.toml", true);
+
+        uint256 chainId = block.chainid;
+        console.log("Deploying to chain: %s", chainId);
 
         // config addresses
         address bayc = config.get("bayc").toAddress();
@@ -30,11 +32,14 @@ contract Setup is Script, Config, Test {
         orderEngine = new OrderEngine();
         vm.stopBroadcast();
 
-        console.log("\nEngine Deployed: ");
+        console.log("\nEngine Deployed: %s", address(orderEngine));
         console.logAddress(address(orderEngine));
 
-        console.log(
-            "\nDeployment complete! Addresses saved to deployments.toml"
-        );
+        console.log("\nDeployment complete! Addresses saved to deployments.toml");
+    }
+
+    function readOwnerOf(address tokenContract, uint256 tokenId) internal {
+        IERC721(tokenContract).ownerOf(tokenId);
+        console.log("Owner of token %s", tokenId);
     }
 }
